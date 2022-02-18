7093df8caf50dde219dd308ae6712043208e2763
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-18 11:18:49 -0500

    open_connection function created

--- jobs/app.py
+++ jobs/app.py
@@ -1,9 +1,16 @@
+from asyncio import open_connection
 from flask import Flask, render_template, g
 import sqlite3
 
 PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None: 
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
 
 @app.route("/")
 @app.route("/jobs")
