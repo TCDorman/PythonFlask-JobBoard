7eded19b8529fe157752cd66933c68750c1c212b
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-18 13:48:18 -0500

    unmuted jobs function

--- jobs/app.py
+++ jobs/app.py
@@ -38,7 +38,7 @@ def jobs():
     return render_template('index.html', jobs=jobs)
 
 
-# @app.route("/job/<job_id>")
-# def jobs(job_id):
-#     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
-#     render_template('job.html', job=job)
+@app.route("/job/<job_id>")
+def jobs(job_id):
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
+    render_template('job.html', job=job)
