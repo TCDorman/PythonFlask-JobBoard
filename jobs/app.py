602131c7964506b81a431d74686b36690c25e23c
Timothy Dorman <94666851+TCDorman@users.noreply.github.com>
2022-02-18 11:40:03 -0500

    close_connection function

--- jobs/app.py
+++ jobs/app.py
@@ -22,6 +22,14 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = cursor.fetchone() if single else cursor.fetchall()
         cursor.close()
         return results
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+    
     
 @app.route("/")
 @app.route("/jobs")
